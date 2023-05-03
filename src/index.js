1299e40ac57bcb46ccf2226d8911b85ec8c9691c
Sameh Refaay <samehrefaay89@gmail.com>
2023-05-03 22:57:34 +0300

    files and folders strucure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+//import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './components/App';
 
-import App from "./components/App";
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-ReactDOM.render(<App />, document.getElementById("root"));
+root.render(
+
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
